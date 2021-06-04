822c47bac7a9b13ced455b8b31c10d7e8c33fcaf
Li-yao Xia <lysxia@gmail.com>
2021-06-04 06:34:53 -0400

    Add spineL

--- src/ExampleLens.v
+++ src/ExampleLens.v
@@ -13,11 +13,12 @@ From Coq Require Import
 Import ListNotations.
 
 From Promonad Require Import
+  Utils
   Promonad.
 
 Record Lens (S U A : Type) : Type := {
   get : S -> option A;
-  put : U -> option (A * (S -> option S) * (S -> bool));
+  put : U -> S -> option (A * S * (S -> bool));
 }.
 
 Arguments get {S U A}.
@@ -26,17 +27,17 @@ Arguments put {S U A}.
 Definition bind {S U A B : Type}
            (m : Lens S U A) (k : A -> Lens S U B) : Lens S U B := {|
   get s := a <- get m s;; get (k a) s;
-  put u :=
-    abc <- put m u;;
-    let '(a, f, p) := abc in
-    def <- put (k a) u;;
-    let '(b, g, q) := def in
-    ret (b, fun x => y <- f x;; g y, fun x => (p x && q x)%bool)
+  put u s :=
+    abc <- put m u s;;
+    let '(a, s, p) := abc in
+    def <- put (k a) u s;;
+    let '(b, s, q) := def in
+    ret (b, s, fun x => (p x && q x)%bool)
 |}.
 
 Definition ret {S U A : Type} (a : A) : Lens S U A := {|
   get _ := Some a;
-  put _ := Some (a, Some, fun _ => true);
+  put _ s := Some (a, s, fun _ => true);
 |}.
 
 Instance Monad_Lens S U : Monad (Lens S U) := {|
@@ -44,9 +45,25 @@ Instance Monad_Lens S U : Monad (Lens S U) := {|
   Promonad.ret _ := ret;
 |}.
 
+Definition map1_triple {a a' b c} (f : a -> a') (x : a * b * c) : a' * b * c :=
+  (f (fst (fst x)), snd (fst x), snd x).
+
+Instance Profunctor_Lens S : Profunctor (Lens S) :=
+  fun _ _ _ _ f g l =>
+    {| get s := option_map g (get l s)
+    ;  put x s := option_map (map1_triple g) (put l (f x) s)
+    |}.
+
+Instance PartialProfunctor_Lens S : PartialProfunctor (Lens S) := {|
+  toFailureP A B (l : Lens S A B) :=
+    {| get s := get l s
+    ;  put ox s := bind_option ox (fun x => put l x s)
+    |}
+|}.
+
 Definition weak_backward {S U A} (m : Lens S U A) : Prop :=
-  forall u a f s' p,
-    put m u = Some (a, f, p) ->
+  forall u a s s_ s' p,
+    put m u s = Some (a, s_, p) ->
     p s' = true ->
     get m s' = Some a.
 
@@ -56,7 +73,7 @@ Lemma weak_backward_bind {S U A B} (m : Lens S U A)
       (Hk : forall a, weak_backward (k a)) :
   weak_backward (bind m k).
 Proof.
-  intros u a f s' p Hu Hp.
+  intros u a s s_ s' p Hu Hp.
   simpl in *.
   destruct (put m u) as [[[]] | ] eqn:eputm; try discriminate; simpl in *.
   destruct (put (k a0) u) as [[[]] | ] eqn:eputk; try discriminate; simpl in *.
@@ -68,10 +85,10 @@ Proof.
 Qed.
 
 Definition weak_forward {S U A} (m : Lens S U A) : Prop :=
-  forall u a f s p,
+  forall u a s s' p,
     get m s = Some a ->
-    put m u = Some (a, f, p) ->
-    f s = Some s.
+    put m u s = Some (a, s', p) ->
+    s = s'.
 
 Lemma weak_forward_bind {S U A B} (m : Lens S U A)
       (k : A -> Lens S U B) (i : B -> option A)
@@ -81,7 +98,7 @@ Lemma weak_forward_bind {S U A B} (m : Lens S U A)
       (Hk : forall a, weak_forward (k a)) :
   weak_forward (bind m k).
 Proof.
-  intros u b f s p Hget Hput.
+  intros u b s s' p Hget Hput.
   simpl in *.
   destruct (get m s) eqn:egetm; try discriminate; simpl in *.
   destruct (put m u) as [[[]] | ] eqn:eputm;
@@ -89,17 +106,95 @@ Proof.
   destruct (put (k a0) u) as [[[]] | ] eqn:eputk;
     try discriminate; simpl in *.
   inversion Hput; subst; clear Hput.
-  pose proof (Hi a).
-  pose proof (Hi a0).
-  apply (f_equal (fun l => get l s)) in H.
-  apply (f_equal (fun l => put l u)) in H0.
-  simpl in *.
-  rewrite Hget in H; simpl in H.
-  rewrite eputk in H0; simpl in H0.
-  inversion H0; clear H0.
-  inversion H; clear H.
-  rewrite H2 in H1; inversion H1; clear H1; subst.
-  replace (o s) with (Some s); [simpl | symmetry; eapply Hm; eauto ].
-  eapply Hk; eauto.
+  assert (Hia := Hi a).
+  assert (Hia0 := Hi a0).
+  apply (f_equal (fun l => get l s)) in Hia.
+  apply (f_equal (fun l => put l u s0)) in Hia0.
+  cbn in *.
+  rewrite Hget in Hia; cbn in Hia.
+  rewrite eputk in Hia0; cbn in Hia0.
+  inversion Hia0; clear Hia0.
+  inversion Hia; clear Hia.
+  rewrite H1 in H0; inversion H0; subst; clear H0.
+  assert (s = s0); [ | subst ].
+  { revert egetm eputm. apply Hm. }
+  revert Hget eputk. red in Hk. apply Hk.
 Qed.
 
+Definition cat {S T U} (l1 : Lens S T T) (l2 : Lens T U U) : Lens S U U :=
+  {| get := fun s => bind_option (get l1 s) (get l2)
+  ;  put := fun x s =>
+      match get l1 s with
+      | None => None
+      | Some t =>
+        bind_option (put l2 x t) (fun '(y, xt, q') =>
+        bind_option (put l1 xt s) (fun '(_, s', p') =>
+        if q' xt then
+          Some (y, s', p')
+        else
+          None))
+      end |}.
+
+Inductive Tree : Type :=
+| Leaf
+| Node : Tree -> nat -> Tree -> Tree
+.
+
+Definition getRoot (t : Tree) : option nat :=
+  match t with
+  | Leaf => None
+  | Node _ n _ => Some n
+  end.
+
+Definition eqb_option {A} (eq : A -> A -> bool) (x y : option A) : bool :=
+  match x, y with
+  | None, None => true
+  | Some x, Some y => eq x y
+  | _, _ => false
+  end.
+
+Definition rootL : Lens Tree (option nat) (option nat) :=
+  {| get t := Some (getRoot t)
+  ;  put n' t :=
+      let t' := match n', t with
+                | None, _ => Leaf
+                | Some n, Leaf => Node Leaf n Leaf
+                | Some n, Node l _ r => Node l n r
+                end in
+      let p t'' := eqb_option Nat.eqb n' (getRoot t'') in
+      Some (n', t', p)
+  |}.
+
+Definition getRight (t : Tree) : option Tree :=
+  match t with
+  | Leaf => None
+  | Node _ _ r => Some r
+  end.
+
+Fixpoint eqb_Tree (t1 t2 : Tree) : bool :=
+  match t1, t2 with
+  | Leaf, Leaf => true
+  | Node l1 n1 r1, Node l2 n2 r2 => eqb_Tree l1 l2 && Nat.eqb n1 n2 && eqb_Tree r1 r2
+  | _, _ => false
+  end.
+
+Definition rightL : Lens Tree Tree Tree :=
+  {| get t := getRight t
+  ;  put r t :=
+      match t with
+      | Leaf => None
+      | Node l n _ => Some (r, Node l n r, fun t' => eqb_option eqb_Tree (Some r) (getRight t'))
+      end
+  |}.
+
+Fixpoint spineL (n : nat) : Lens Tree (list nat) (list nat) :=
+  match n with
+  | O => bind (comap (fun _ => Some None) rootL) (fun _ => ret nil)
+  | S n =>
+    bind (comap (fun xs => Some (head xs)) rootL) (fun hd =>
+    match hd with
+    | None => ret nil
+    | Some h => bind (comap tail (cat rightL (spineL n))) (fun tl =>
+                ret (h :: tl))
+    end)
+  end.
