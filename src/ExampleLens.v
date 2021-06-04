54b245dfac201ba72b96f8a825723da51dcc0d7d
Li-yao Xia <lysxia@gmail.com>
2021-06-04 07:49:16 -0400

    ExampleLens: Quasicompositionality

--- src/ExampleLens.v
+++ src/ExampleLens.v
@@ -61,65 +61,10 @@ Instance PartialProfunctor_Lens S : PartialProfunctor (Lens S) := {|
     |}
 |}.
 
-Definition weak_backward {S U A} (m : Lens S U A) : Prop :=
-  forall u a s s_ s' p,
-    put m u s = Some (a, s_, p) ->
-    p s' = true ->
-    get m s' = Some a.
-
-Lemma weak_backward_bind {S U A B} (m : Lens S U A)
-      (k : A -> Lens S U B)
-      (Hm : weak_backward m)
-      (Hk : forall a, weak_backward (k a)) :
-  weak_backward (bind m k).
+Instance Promonad_Lens S : Promonad (Lens S).
 Proof.
-  intros u a s s_ s' p Hu Hp.
-  simpl in *.
-  destruct (put m u) as [[[]] | ] eqn:eputm; try discriminate; simpl in *.
-  destruct (put (k a0) u) as [[[]] | ] eqn:eputk; try discriminate; simpl in *.
-  inversion Hu; subst; clear Hu.
-  destruct (andb_prop _ _ Hp).
-  replace (get m s') with (Some a0).
-  { simpl. eapply Hk; eauto. }
-  { symmetry. eapply Hm; eauto. }
-Qed.
-
-Definition weak_forward {S U A} (m : Lens S U A) : Prop :=
-  forall u a s s' p,
-    get m s = Some a ->
-    put m u s = Some (a, s', p) ->
-    s = s'.
-
-Lemma weak_forward_bind {S U A B} (m : Lens S U A)
-      (k : A -> Lens S U B) (i : B -> option A)
-      (Hi : forall a,
-          (x <- k a;; ret (i x)) = (x <- k a;; ret (Some a)))
-      (Hm : weak_forward m)
-      (Hk : forall a, weak_forward (k a)) :
-  weak_forward (bind m k).
-Proof.
-  intros u b s s' p Hget Hput.
-  simpl in *.
-  destruct (get m s) eqn:egetm; try discriminate; simpl in *.
-  destruct (put m u) as [[[]] | ] eqn:eputm;
-    try discriminate; simpl in *.
-  destruct (put (k a0) u) as [[[]] | ] eqn:eputk;
-    try discriminate; simpl in *.
-  inversion Hput; subst; clear Hput.
-  assert (Hia := Hi a).
-  assert (Hia0 := Hi a0).
-  apply (f_equal (fun l => get l s)) in Hia.
-  apply (f_equal (fun l => put l u s0)) in Hia0.
-  cbn in *.
-  rewrite Hget in Hia; cbn in Hia.
-  rewrite eputk in Hia0; cbn in Hia0.
-  inversion Hia0; clear Hia0.
-  inversion Hia; clear Hia.
-  rewrite H1 in H0; inversion H0; subst; clear H0.
-  assert (s = s0); [ | subst ].
-  { revert egetm eputm. apply Hm. }
-  revert Hget eputk. red in Hk. apply Hk.
-Qed.
+  constructor; typeclasses eauto.
+Defined.
 
 Definition cat {S T U} (l1 : Lens S T T) (l2 : Lens T U U) : Lens S U U :=
   {| get := fun s => bind_option (get l1 s) (get l2)
@@ -198,3 +143,163 @@ Fixpoint spineL (n : nat) : Lens Tree (list nat) (list nat) :=
                 ret (h :: tl))
     end)
   end.
+
+Definition t0 : Tree := Node (Node Leaf 0 Leaf) 1 (Node Leaf 2 Leaf).
+
+Lemma example_0
+ : map fst (put (spineL 3) [3; 4; 5] t0)
+ = Some ([3; 4; 5], Node (Node Leaf 0 Leaf) 3 (Node Leaf 4 (Node Leaf 5 Leaf))).
+Proof. reflexivity. Qed.
+
+(**)
+
+Instance MonadLaws_Lens {S U} : MonadLaws (Lens S U).
+Proof.
+  constructor; cbn.
+  - intros ? []; unfold bind; cbn.
+    f_equal.
+    + apply functional_extensionality; intros. exact (bind_ret (M := option) _ _).
+    + do 2 (apply functional_extensionality; intros).
+      destruct (put0 x x0) as [ [[] ?] | ]; cbn; f_equal.
+      f_equal. apply functional_extensionality; intros ?. rewrite Bool.andb_true_r. reflexivity.
+  - intros ? ? ? ?; unfold bind; cbn.
+    destruct (k a); cbn; f_equal.
+    do 2 (apply functional_extensionality; intros).
+    destruct (put0 x x0) as [ [[] ?] | ]; cbn; f_equal.
+  - intros; unfold bind; cbn. f_equal.
+    + apply functional_extensionality; intros; exact (bind_bind (M := option) _ _ _ _ _ _).
+    + do 2 (apply functional_extensionality; intros).
+      destruct (put m x x0) as [ [[] ?] | ]; cbn; [ | reflexivity ].
+      destruct (put (k a) x s) as [ [[] ?] | ]; cbn; [ | reflexivity ].
+      destruct (put (h b0) x s0) as [ [[] ?] | ]; cbn; f_equal. f_equal.
+      apply functional_extensionality; intros; rewrite Bool.andb_assoc. reflexivity.
+Qed.
+
+Instance ProfunctorLaws_Lens {S} : ProfunctorLaws (Lens S).
+Proof.
+  constructor; cbn.
+  - intros; apply functional_extensionality; intros []; unfold dimap, Profunctor_Lens, id; cbn.
+    f_equal.
+    + apply functional_extensionality. intros; destruct (get0 _); reflexivity.
+    + do 2 (apply functional_extensionality; intros).
+      destruct (put0 _ _) as [ [[] ? ] | ]; reflexivity.
+  - intros; apply functional_extensionality; intros []; unfold dimap, Profunctor_Lens, compose; cbn.
+    f_equal.
+    + apply functional_extensionality. intros; destruct (get0 _); reflexivity.
+    + do 2 (apply functional_extensionality; intros).
+      destruct (put0 _ _) as [ [[] ? ] | ]; reflexivity.
+Qed.
+
+Instance PartialProfunctorLaws_Lens {S} : PartialProfunctorLaws (Lens S).
+Proof.
+  constructor; cbn.
+  - typeclasses eauto.
+  - intros; unfold Profunctor_Lens; cbn.
+    f_equal. do 2 (apply functional_extensionality; intros).
+    destruct x; cbn; reflexivity.
+Qed.
+
+Instance comap_morphism_Lens {S U V : Type} (f : U -> V)
+  : MonadMorphism  (M := Lens S V) (N := Lens S U)
+      (fun A : Type => comap (fun u : U => Some (f u))).
+Proof.
+  constructor; cbn; unfold Profunctor_Lens; cbn.
+  - intros; unfold ret; cbn. f_equal.
+  - intros; unfold bind; cbn. f_equal.
+    + apply functional_extensionality; intros.
+      destruct get; cbn; reflexivity.
+    + do 2 (apply functional_extensionality; intros).
+      destruct put as [ [[] ? ] | ]; cbn; [ | reflexivity ].
+      destruct put as [ [[] ? ] | ]; cbn; reflexivity.
+Qed.
+
+Instance PromonadLaws_Lens {S} : PromonadLaws (Lens S).
+Proof.
+  constructor; cbn; typeclasses eauto.
+Qed.
+
+Definition weak_backward {S U A} (m : Lens S U A) : Prop :=
+  forall u a s s_ s' p,
+    put m u s = Some (a, s_, p) ->
+    p s' = true ->
+    get m s' = Some a.
+
+Lemma weak_backward_bind {S U A B} (m : Lens S U A)
+      (k : A -> Lens S U B)
+      (Hm : weak_backward m)
+      (Hk : forall a, weak_backward (k a)) :
+  weak_backward (bind m k).
+Proof.
+  intros u a s s_ s' p Hu Hp.
+  simpl in *.
+  destruct (put m u) as [[[]] | ] eqn:eputm; try discriminate; simpl in *.
+  destruct (put (k a0) u) as [[[]] | ] eqn:eputk; try discriminate; simpl in *.
+  inversion Hu; subst; clear Hu.
+  destruct (andb_prop _ _ Hp).
+  replace (get m s') with (Some a0).
+  { simpl. eapply Hk; eauto. }
+  { symmetry. eapply Hm; eauto. }
+Qed.
+
+Lemma Compositional_weak_backward {S} : Compositional (@weak_backward S).
+Proof.
+  constructor; cbn.
+  - typeclasses eauto.
+  - red; cbn; intros. inversion H; auto.
+  - apply @weak_backward_bind.
+  - unfold weak_backward, Profunctor_Lens; cbn; intros.
+    destruct (f u) eqn:Ef; cbn in H0; [ | discriminate ].
+    destruct put as [ [[] ?] | ] eqn:Eput; unfold map1_triple in H0; cbn in H0; [ | discriminate].
+    inversion H0; subst; clear H0.
+    erewrite H; eauto; reflexivity.
+Qed.
+
+Definition weak_forward {S U A} (m : Lens S U A) : Prop :=
+  forall u a s s' p,
+    get m s = Some a ->
+    put m u s = Some (a, s', p) ->
+    s = s'.
+
+Lemma weak_forward_bind {S U A B} (m : Lens S U A)
+      (k : A -> Lens S U B) (i : B -> option A)
+      (Hi : forall a,
+          (x <- k a;; ret (i x)) = (x <- k a;; ret (Some a)))
+      (Hm : weak_forward m)
+      (Hk : forall a, weak_forward (k a)) :
+  weak_forward (bind m k).
+Proof.
+  intros u b s s' p Hget Hput.
+  simpl in *.
+  destruct (get m s) eqn:egetm; try discriminate; simpl in *.
+  destruct (put m u) as [[[]] | ] eqn:eputm;
+    try discriminate; simpl in *.
+  destruct (put (k a0) u) as [[[]] | ] eqn:eputk;
+    try discriminate; simpl in *.
+  inversion Hput; subst; clear Hput.
+  assert (Hia := Hi a).
+  assert (Hia0 := Hi a0).
+  apply (f_equal (fun l => get l s)) in Hia.
+  apply (f_equal (fun l => put l u s0)) in Hia0.
+  cbn in *.
+  rewrite Hget in Hia; cbn in Hia.
+  rewrite eputk in Hia0; cbn in Hia0.
+  inversion Hia0; clear Hia0.
+  inversion Hia; clear Hia.
+  rewrite H1 in H0; inversion H0; subst; clear H0.
+  assert (s = s0); [ | subst ].
+  { revert egetm eputm. apply Hm. }
+  revert Hget eputk. red in Hk. apply Hk.
+Qed.
+
+Lemma Quasicompositional_weak_forward {S} : Quasicompositional (@weak_forward S).
+Proof.
+  constructor; cbn.
+  - red; cbn; intros. inversion H0; auto.
+  - exact (@weak_forward_bind S).
+  - unfold weak_forward, Profunctor_Lens; cbn; intros.
+    destruct (get m s) eqn:Hget; cbn in H0; inversion H0; subst; clear H0.
+    destruct (f u) eqn:Hf; cbn in H1; [ | discriminate ].
+    destruct put as [ [[] ?] | ] eqn:Hput; unfold map1_triple in H1; cbn in H1; [ | discriminate ].
+    inversion H1; subst; clear H1.
+    revert Hget Hput; apply H.
+Qed.
